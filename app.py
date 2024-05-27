226f22a52d7f04cbb2436aa6c3882f0790a03198
gh9727 <gj9727@gmail.com>
2024-05-27 10:34:28 +0900

    mask 이미지 생성 기능 구현

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ def home():
 def opencv():
     # 이미지 파일 열기
     image_path = 'static/jeremih.png'
-    image = cv2.imread(image_path)
-    
-    # 이미지를 JPEG 포맷으로 메모리 스트림에 저장
-    is_success, buffer = cv2.imencode(".png", image)
+    image = cv2.imread(image_path,0) # -> 일반 이미지 + 0 은 이미지를 흑백으로 불러옴
+
+    th,mask = cv2.threshold(image,0,255,cv2.THRESH_OTSU)
+    # 이미지를 png 포맷으로 메모리 스트림에 저장
+    is_success, buffer = cv2.imencode(".png", mask)
     io_buf = io.BytesIO(buffer)
+
 
     # 이미지 파일을 HTTP 응답으로 반환
     return send_file(io_buf, mimetype='image/png', as_attachment=False, download_name='jeremih.png')
